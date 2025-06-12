eda204b18f78ba1623fba482b66cb80c7f89bb93
Sigma <allankirimi65@gmail.com.>
2025-06-12 16:28:16 +0300

    fixed the env variable

--- src/database/config.js
+++ src/database/config.js
@@ -1,6 +1,7 @@
 //this is where we are able to make a connection to our mongodb database
-
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+dotenv.config();
 export const connectMongo = async () => {
   try {
     await mongoose.connect(process.env.CONNECTION_STRING);
